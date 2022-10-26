75cf42ec438cb950f211c1d814e598b16d955622
uziel78 <rune.husa78@gmail.com>
2022-10-26 22:22:10 +0200

    Starting Section 12

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
+### About
+
+This project is part two of a course found on Udemy (https://www.udemy.com/course/nextjs-dev-to-deployment/). It deals with how Markdown Files are incorporeated into a blog-post type project. The following technology is used:
+
+- Next.js
+- Tailwind (styling)
+- fs module (node.js module to enable use of local files to replace API data);
+  useful tip: https://github.com/vercel/next.js/discussions/12124
+- path module (module provides utilities for working with file and directory paths).
+- grey-matter (parses a string with front-matter into an object)
+- markdown files/syntax
+- marked (package used to parse the markdown content)
+
+**The project also handles:**
+
+- sorting by date
+- category labeling (specific colors added for specific categories of content)
+- pagination
+
 ## Getting Started
 
 First, run the development server:
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,60 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import Link from 'next/link';
 import Layout from '../components/Layout';
+import Post from '../components/Post';
+import { sortByDate } from '../utils';
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
+  //console log client-side
+  console.log(posts);
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latests Posts</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-800 focus:outline-none focus:shadow-outline w-full'>
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
+}
+
+// accesses markdown files in posts folder, creates a slug and creates an array of objects to render
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    //console.log(markdownWithMeta);
+    //use grey-matter to parse file content into objects (data object also renamed to frontmatter)
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  //console.log(posts);
+
+  //uses function in utils to help sort by date, then only render six in total
+  return {
+    props: {
+      posts: posts.sort(sortByDate).slice(0, 6),
+    },
+  };
 }
